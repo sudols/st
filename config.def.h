2f94724a36beea4c3575b98cdc49eab1aa096457
noid0 <zeus.akp.06@gmail.com>
2022-09-17 19:14:58 +0530

    Revert to "patch st-hidecursor"
    This reverts commit c53a946467e589a767dd46f85e22bc20040ab949.

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
-	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
 
 static int borderpx = 2;
